6307b215ee1bd8ecd329cb73d8ba5ae18069cdd7
ahcene <ahcene.dahoumane@u-psud.fr>
2022-04-22 15:55:03 +0200

    finishJoueur

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -4,30 +4,76 @@ package modele;
 import java.util.ArrayList;
 
 enum role {normal , ingenieur , plongeur , messager};
+
 public class Joueur {
-    private  int x;
+    private int x;
     private int y;
-    role r ;
+    role r;
 
     public ArrayList<artefact> artefactList;
-    public ArrayList<element>
+    public ArrayList<element> artfactElementList;
 
 
-    public Joueur(int x, int y,role r) {
+    public Joueur(int x, int y, role r) {
         this.x = x;
         this.y = y;
         this.r = r;
     }
 
-    public void assigneRole() {
-        if (Grade == "pilote") {
-        } else if (Grade == "Navigateur") {
-        } else if (Grade == "Plongeur") {
-        } else if (Grade == "Explorateur") {
-        } else if (Grade == "Ingenieur") {
-        } else if (Grade == "Messager") {
-            {
+    public void addArtefact(artefact k) {
+        artefactList.add(k);
+        artfactElementList.add(k.getElement());
+    }
+    public boolean getArtefact(artefact k){
+        return artefactList.contains(k);
+    }
+    public int nbOfArtefact(element e) {
+        int res = 0;
+        for (artefact k : artefactList) {
+            if (k.getElement() == e) {
+                res += 1;
             }
         }
+        return res;
     }
+    public void afficheArtefact(){
+            if(artefactList.isEmpty()) {
+                System.out.println("Le joueur ne possede pas d'artefact");
+            }else{
+                System.out.println("Il possede les artefact suivants");
+                for(element e : element.values()){
+                    int nbArtefact =nbOfArtefact(e);
+                    System.out.println(nbArtefact  +" "+ e + " ");
+                }
+
+            }
+
+    }
+        public int getX() {
+            return x;
+        }
+
+
+        public int getY() {
+            return y;
+        }
+
+
+        public role getRole() {
+            return r;
+        }
+
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public void setRole(role r) {
+            this.r = r;
+        }
 }
--- src/modele/artefact.java
+++ src/modele/artefact.java
@@ -3,8 +3,16 @@ package modele;
 enum element{air,eau,feu,terre}
 public class artefact {
     private element e;
-public artefact(element e ){
+public artefact(element e){
     this.e= e ;
 }
-
+public artefact(typeZone t){
+    if(t == typeZone.air) this.e = element.air;
+    if(t == typeZone.eau) this.e = element.eau;
+    if(t == typeZone.terre) this.e = element.terre;
+    if(t == typeZone.feu) this.e = element.feu;
+}
+public element getElement(){
+    return e ;
+}
 }
