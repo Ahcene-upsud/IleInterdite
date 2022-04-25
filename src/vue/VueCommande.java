50f650b333fdbace2bca59d0b53ecdd4c85147ad
soufian91 <soufian.msallem@u-psud.fr>
2022-04-25 04:15:42 +0100

    fin du projet

--- src/vue/VueCommande.java
+++
@@ -1,9 +0,0 @@
-package vue;
-import modele.Modele;
-
-import javax.swing.*;
-
-public class VueCommande extends JPanel {
-    private Modele
-
-}
--- src/vue/VuePlateau.java
+++
@@ -1,6 +0,0 @@
-package vue;
-import javax.swing.*;
-import java.util.Observer;
-
-public class VuePlateau extends JPanel implements Observer {
-}
